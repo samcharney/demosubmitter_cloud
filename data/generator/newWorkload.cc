4fd67af076d7541091bcd70df6582e8105d5f2e7
Sam Charney <sammycharney@gmail.com>
2021-05-31 21:11:27 -0400

    generating key value pairs for data file

--- data/generator/newWorkload.cc
+++ data/generator/newWorkload.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cmath>
 #include <cstdlib>
@@ -6,6 +7,7 @@
 #include <iomanip>
 #include <cstdint>
 #include <cassert>
+#include <limits>
 
 class params{
 	public:
@@ -18,6 +20,9 @@ class params{
         unsigned int numNonEmptyRangeLookups;
         unsigned int numEmptyRangeLookups;
 	bool isUniform;
+	unsigned int maxKey;
+	std::ofstream bulkdata;
+	std::ofstream workload;
 
 	params() {
 		numKeys=1000*1000*10;
@@ -29,7 +34,14 @@ class params{
 	        numNonEmptyRangeLookups=10;
 	        numEmptyRangeLookups=10;
         	isUniform=true;
+		maxKey=INT_MAX;
+		bulkdata.open("bulkwrite.txt");
+		workload.open("workload.txt");
+	}
 
+	void close() {
+		bulkdata.close();
+		workload.close();
 	}
 };
 
@@ -50,6 +62,32 @@ void help() {
 		std::cout<<str<<std::endl;
 	}
 	
+}
+
+std::vector<int> keys;
+//std::vector<unsigned int> values;
+//In the original program, values was not used and a bizarre string of A's was printed to the file
+
+void generateKeys(params& args) {
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    	std::mt19937_64 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    	std::uniform_int_distribution<int> uni_dist1(-args.maxKey, args.maxKey);
+   	std::uniform_int_distribution<unsigned int> uni_dist2(1, 10000); //should 10000 be a param?
+	
+	keys.reserve(args.numKeys);
+	//values.reserve(args.numKeys);
+	
+	for (int i=0; i<args.numKeys; i++) {
+		int key=uni_dist1(gen);
+		unsigned int val=uni_dist2(gen); //is it okay to pull values out of the same generator?
+		keys.push_back(key);
+        	//values.push_back(val);
+		args.bulkdata << key << " " << val <<std::endl; //should this be A's?
+	}
+}
+
+void generateWorkload(params& args) {
+
 }
 
 int main(int argc, char* argv[]) {
@@ -104,6 +142,11 @@ int main(int argc, char* argv[]) {
 			assert(i<argc);
 			args.numEmptyRangeLookups=atoi(argv[i]);
                 }
+                else if (p=="-maxKey") {
+                	i++;
+			assert(i<argc);
+			args.maxKey=atoi(argv[i]);
+                }
                 else if (p=="-h") {
                 	help();
 			return 0;
@@ -115,5 +158,9 @@ int main(int argc, char* argv[]) {
 
 	}
 	//do work here
+	
+	generateKeys(args);
+	generateWorkload(args);
+	args.close();
 	return 0;
 }
