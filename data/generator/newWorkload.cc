3d8b71231fd914b17b138ea859d9bc3533ec1ea7
Sam Charney <sammycharney@gmail.com>
2021-06-21 20:55:13 -0400

    Reindented code

--- data/generator/newWorkload.cc
+++ data/generator/newWorkload.cc
@@ -28,14 +28,14 @@ class params{
         unsigned int numNonEmptyRangeLookups;
         unsigned int numEmptyRangeLookups;
         unsigned int skewBoundary;
-		unsigned int rangeLength;
+        unsigned int rangeLength;
         double skewProb;
         bool isUniform;
         keytype maxKey;
         std::ofstream bulkdata;
         std::ofstream workload;
-//Default values which can also be entered by the user 
-params() {
+        //Default values which can also be entered by the user 
+        params() {
             numKeys=1000*1000*10;
             numPointLookups=10;
             numZeroResultPointLookups=10;
@@ -45,7 +45,7 @@ params() {
             numNonEmptyRangeLookups=10;
             numEmptyRangeLookups=10;
             skewBoundary=1000;
-			rangeLength=100;
+            rangeLength=100;
             skewProb=0.5;
             isUniform=true;
             maxKey=INT_MAX;
@@ -85,7 +85,7 @@ void help() {
         "-m maxKey",
         "-s skewBoundary",
         "-p skewProbability",
-		"-rl rangeLength"
+        "-rl rangeLength"
     };
     for(auto str:s) {
         std::cout << '\t' << str << '\n';
@@ -99,8 +99,8 @@ template <typename T>
 class random_numbers {
     std::mt19937 gen;
     /*
-     If doing uniform distribution, use distribU on entire range.
-     If doing skew distribution, use choose_distrib to pick between distrib1 and distrib2, set to the skew ranges
+       If doing uniform distribution, use distribU on entire range.
+       If doing skew distribution, use choose_distrib to pick between distrib1 and distrib2, set to the skew ranges
      */
     std::uniform_int_distribution<T> distribU;
     std::uniform_int_distribution<T> distrib1;
@@ -108,15 +108,15 @@ class random_numbers {
     std::binomial_distribution<> choose_distrib;
     distrib_enum distrib_type;
 
-	//Class takes the lower and upper bounds, the probability of choosing between the skewed ranges,
+    //Class takes the lower and upper bounds, the probability of choosing between the skewed ranges,
     //the divider of the skewed ranges, and whether or not it is a skew distribution,
     //with the last three values having defaults to make it easy to create a uniform distribution which does not need any of them.
     public:
     random_numbers(T lo, T hi, double skewProb = 0.5, T bound = 0, bool isUniform = true) : distribU(lo, hi),
-                                          distrib1(lo, std::max(lo, bound)),
-                                          distrib2(std::min(bound, hi), hi),
-                                          choose_distrib(1, skewProb), 
-                                          gen(get_generator()) 
+    distrib1(lo, std::max(lo, bound)),
+    distrib2(std::min(bound, hi), hi),
+    choose_distrib(1, skewProb), 
+    gen(get_generator()) 
     {
         if (isUniform) {
             distrib_type=dUniform;   
@@ -156,7 +156,7 @@ std::vector<valtype> values;
 
 //Generates the appropriate number of key value pairs to fill the data file
 void generateKeys(params& args) {
-   
+
     random_numbers<keytype> key_gen(-args.maxKey, args.maxKey, args.skewProb, args.skewBoundary, args.isUniform);
 
     keys.reserve(args.numKeys);
@@ -179,8 +179,8 @@ void generateKeys(params& args) {
 void generateWorkload(params& args) {
     std::vector<char> op_order(args.num_ops());
 
-	//Begins by creating a list of the operations the number of times it should appear 
-	//and then takes a permutation of that list so they are in a random order
+    //Begins by creating a list of the operations the number of times it should appear 
+    //and then takes a permutation of that list so they are in a random order
     unsigned int a1 = args.numPointLookups;
     unsigned int a2 = args.numZeroResultPointLookups +a1;
     unsigned int a3 = args.numInserts +a2;
@@ -224,21 +224,21 @@ void generateWorkload(params& args) {
     //generates random permutation 
     std::shuffle(op_order.begin(), op_order.end(), g);
 
-	//FIXME When new keys are added from inserts, they will not be called for subsequent 
-	//point lookups, zero result point lookups, blind updates, or read modify updates
+    //FIXME When new keys are added from inserts, they will not be called for subsequent 
+    //point lookups, zero result point lookups, blind updates, or read modify updates
     random_numbers<keytype> old_keys(0, keys.size()-1);
 
     random_numbers<keytype> new_keys(-args.maxKey, args.maxKey, args.skewProb, args.skewBoundary, args.isUniform);
-	
-	//Subtracting off range length to ensure the range lies completely within the key range
-	random_numbers<keytype> range_start(-args.maxKey, args.maxKey-args.rangeLength);
 
-	unsigned int rangeFailures=0;
+    //Subtracting off range length to ensure the range lies completely within the key range
+    random_numbers<keytype> range_start(-args.maxKey, args.maxKey-args.rangeLength);
+
+    unsigned int rangeFailures=0;
 
     for(int i=0; i<op_order.size(); i++) {
         char op=op_order[i];
         switch(op) {
-			//Point lookups and zero result point lookups only need a key
+            //Point lookups and zero result point lookups only need a key
             case 'l':
             case 'z': 
                 {
@@ -246,7 +246,7 @@ void generateWorkload(params& args) {
                     args.workload << op  << ' ' << keys[index] << '\n';
                     break;
                 }
-			//Inserts find a new key and add it and a value to the list
+                //Inserts find a new key and add it and a value to the list
             case 'i': 
                 {
                     keytype new_key;
@@ -258,13 +258,13 @@ void generateWorkload(params& args) {
                         }
                     } while(!isNew);
                     valtype val = "AAAAAAAAAAA"; //default value. Values aren't actually used
-        			keys.push_back(new_key);
-        			keys_set.insert(new_key);
-        			values.push_back(val);
+                    keys.push_back(new_key);
+                    keys_set.insert(new_key);
+                    values.push_back(val);
                     args.workload << op << ' ' << new_key << ' ' << val << '\n';
                     break;
                 }
-			//Blind updates find an old key and change the value, but values aren't used in this program so it is a default
+                //Blind updates find an old key and change the value, but values aren't used in this program so it is a default
             case 'u':
                 {
                     unsigned int index = old_keys.get_random();
@@ -272,7 +272,7 @@ void generateWorkload(params& args) {
                     args.workload << op  << ' ' << keys[index] << ' ' << val << '\n';
                 }
                 break;
-			//Read modify updates find an old key and increment the value, but values aren't use in this program so it is a default
+                //Read modify updates find an old key and increment the value, but values aren't use in this program so it is a default
             case 'w':
                 {
                     unsigned int index = old_keys.get_random();
@@ -280,46 +280,46 @@ void generateWorkload(params& args) {
                     args.workload << op  << ' ' << keys[index] << ' ' << incremented_val << '\n';
                 }
                 break;
-			//Non empty range queries finds a range of keys where at least one of the indices is an existing key 
+                //Non empty range queries finds a range of keys where at least one of the indices is an existing key 
             case 'r':
-				{
-					keytype rangeS;
-					bool foundRange=false;
-					do {
-						rangeS = range_start.get_random();
-						keytype rangeE = rangeS + args.rangeLength;
-						for (keytype i=rangeS; i<rangeE; i++) {
-                        	if (keys_set.find(i)!=keys_set.end()) {
-                            	foundRange=true;
-                        	}
-						}
-					} while(!foundRange);
-					args.workload << op << ' ' << rangeS << ' ' << args.rangeLength << '\n';
-				}
-				break;
-			//Empty range queries finds a range of keys where none of them are used, printing an error message if no ranges can be found
+                {
+                    keytype rangeS;
+                    bool foundRange=false;
+                    do {
+                        rangeS = range_start.get_random();
+                        keytype rangeE = rangeS + args.rangeLength;
+                        for (keytype i=rangeS; i<rangeE; i++) {
+                            if (keys_set.find(i)!=keys_set.end()) {
+                                foundRange=true;
+                            }
+                        }
+                    } while(!foundRange);
+                    args.workload << op << ' ' << rangeS << ' ' << args.rangeLength << '\n';
+                }
+                break;
+                //Empty range queries finds a range of keys where none of them are used, printing an error message if no ranges can be found
             case 'e':
                 {
-					keytype rangeS;
-					bool foundRange=true;
-					int tries=0;
-					int maxTries=10 + args.maxKey/100; //max number of tries is at least 10 and is a function of the size of the key space
-					do {
-						tries++;
-						rangeS = range_start.get_random();
-						keytype rangeE = rangeS + args.rangeLength;
-						for (keytype i=rangeS; i<rangeE; i++) {
-                        	if (keys_set.find(i)!=keys_set.end()) {
-                            	foundRange=false;
-                        	}
-						}
-					} while(foundRange && tries < maxTries);
-					if (foundRange) {
-						args.workload << op << ' ' << rangeS << ' ' << args.rangeLength << '\n';
-					}
-					else {
-						rangeFailures++;
-					}
+                    keytype rangeS;
+                    bool foundRange=true;
+                    int tries=0;
+                    int maxTries=10 + args.maxKey/100; //max number of tries is at least 10 and is a function of the size of the key space
+                    do {
+                        tries++;
+                        rangeS = range_start.get_random();
+                        keytype rangeE = rangeS + args.rangeLength;
+                        for (keytype i=rangeS; i<rangeE; i++) {
+                            if (keys_set.find(i)!=keys_set.end()) {
+                                foundRange=false;
+                            }
+                        }
+                    } while(foundRange && tries < maxTries);
+                    if (foundRange) {
+                        args.workload << op << ' ' << rangeS << ' ' << args.rangeLength << '\n';
+                    }
+                    else {
+                        rangeFailures++;
+                    }
                 }
                 break;
             default: 
@@ -328,16 +328,16 @@ void generateWorkload(params& args) {
 
         } //switch
     } //for
-	if (rangeFailures != 0) {
-		std::cout << "Empty range could not be found " << rangeFailures << " times\n";
-	}
+    if (rangeFailures != 0) {
+        std::cout << "Empty range could not be found " << rangeFailures << " times\n";
+    }
 }
 
 int main(int argc, char* argv[]) {
 
     params args;
 
-	//Parses the commands the user enters and updates the parameters as necessary
+    //Parses the commands the user enters and updates the parameters as necessary
     for(int i=1; i<argc; i++) {
         std::string p=argv[i];
         if (p=="-uniform") {
@@ -401,11 +401,11 @@ int main(int argc, char* argv[]) {
             assert(i<argc);
             args.skewProb=atof(argv[i]);
         }
-		else if (p=="-rl") {
-			i++;
-			assert(i<argc);
-			args.rangeLength=atoi(argv[i]);
-		}
+        else if (p=="-rl") {
+            i++;
+            assert(i<argc);
+            args.rangeLength=atoi(argv[i]);
+        }
         else if (p=="-h") {
             help();
             return 0;
@@ -417,7 +417,7 @@ int main(int argc, char* argv[]) {
 
     }
 
-	//Generates the data and workload files
+    //Generates the data and workload files
     generateKeys(args);
     generateWorkload(args);
     args.close();
